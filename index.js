bc8f0ceac754e3f9866c14ac1d1ec26fe6826811
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-22 01:39:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector)
+}
+function nestedTarget()
+{
+  return document.querySelector('#nested .target')
+}
+function deepestChild()
+{
+  return document.querySelector('#grand-node div div div div')
+}
+function increaseRankBy(n)
+{
+  var rankList = document.querySelectorAll('.ranked-list')
+  for (var i = 0; i< rankList.length; i++)
+    {
+      rankList[i].innerHTML = parseInt(rankList[i].innerHTML) + n
+    }
+}
